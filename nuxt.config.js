238e76e9b4cc461d92cfc6c2f9a1bfd82ac47dbe
Phạm Nguyễn Thuận <tphamnguyen@ploggmedia.com>
2022-05-10 20:33:42 +0700

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,10 @@
+import colors from 'vuetify/es5/util/colors'
+
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'testnew',
+    titleTemplate: '%s - testfilevuetify',
+    title: 'testfilevuetify',
     htmlAttrs: {
       lang: 'en'
     },
@@ -29,11 +32,42 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
+    // https://go.nuxtjs.dev/vuetify
+    '@nuxtjs/vuetify',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
   ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+    baseURL: '/',
+  },
+
+  // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+
+    theme: {
+      dark: true,
+      treeShaking: true,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        }
+      }
+    }
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
--- pages/index.vue
+++ pages/index.vue
@@ -1,183 +1,209 @@
 <template>
-  <div id="app"></div>
+	<v-layout fill-frame ref="player3D"> </v-layout>
 </template>
 
 <script>
-import * as THREE from '@plogg/three';
-import { ArToolkitProfile, ArToolkitSource, ArToolkitContext, ArMarkerControls} from '@ar-js-org/ar.js/three.js/build/ar-threex';
+import * as THREE from "@plogg/three";
+import {
+	ArToolkitProfile,
+	ArToolkitSource,
+	ArToolkitContext,
+	ArMarkerControls,
+} from "@ar-js-org/ar.js/three.js/build/ar-threex";
 
 export default {
-  name: "ARScene",
-  mounted() {
-    ArToolkitContext.baseURL = './'
-    console.log("ARScene mounted");
+	name: "ARScene",
+	data() {
+		return {
+			renderer: null,
+			camera: null,
+			animate: null,
+			scene: null,
+		};
+	},
+	mounted() {
+		ArToolkitContext.baseURL = "./";
+		console.log("ARScene mounted");
 
-    // init renderer
-    var renderer	= new THREE.WebGLRenderer({
-      antialias	: true,
-      alpha: true
-    });
+		// init renderer
+		this.renderer = new THREE.WebGLRenderer({
+			antialias: true,
+			alpha: true,
+		});
 
-    renderer.setClearColor(new THREE.Color('lightgrey'), 0)
-    // renderer.setPixelRatio( 2 );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    renderer.domElement.style.position = 'absolute'
-    renderer.domElement.style.top = '0px'
-    renderer.domElement.style.left = '0px'
-    document.body.appendChild( renderer.domElement ); // We should be able to specify an html element to append AR.js related elements to.
+		this.renderer.setClearColor(new THREE.Color("lightgrey"), 0);
+		// renderer.setPixelRatio( 2 );
+		this.renderer.setSize(window.innerWidth, window.innerHeight);
+		this.renderer.domElement.style.position = "absolute";
+		this.renderer.domElement.style.top = "0px";
+		this.renderer.domElement.style.left = "0px";
+		this.$refs.player3D.appendChild(this.renderer.domElement); // We should be able to specify an html element to append AR.js related elements to.
+		console.log(window.innerWidth, " ", window.innerHeight);
+		// array of functions for the rendering loop
+		var onRenderFcts = [];
 
-    // array of functions for the rendering loop
-    var onRenderFcts= [];
+		// init scene and camera
+		this.scene = new THREE.Scene();
 
-    // init scene and camera
-    var scene	= new THREE.Scene();
+		//////////////////////////////////////////////////////////////////////////////////
+		//		Initialize a basic camera
+		//////////////////////////////////////////////////////////////////////////////////
 
-    //////////////////////////////////////////////////////////////////////////////////
-    //		Initialize a basic camera
-    //////////////////////////////////////////////////////////////////////////////////
+		// Create a camera
+		var camera = new THREE.Camera();
+		this.scene.add(camera);
+		const artoolkitProfile = new ArToolkitProfile();
+		artoolkitProfile.sourceWebcam(); // Is there good reason for having a function to set the sourceWebcam but not the displayWidth/Height etc?
+		console.log(artoolkitProfile.sourceParameters); // now includes the additionalParameters
+		// add existing parameters, this is not well documented
+		let additionalParameters = {
+			// Device id of the camera to use (optional)
+			deviceId: null,
+			// resolution of at which we initialize in the source image
+			sourceWidth: 640,
+			sourceHeight: 480,
+			// resolution displayed for the source
+			displayWidth: window.innerWidth,
+			displayHeight: window.innerHeight,
+		};
 
-    // Create a camera
-    var camera = new THREE.Camera();
-    scene.add(camera);
-    const artoolkitProfile = new ArToolkitProfile();
-    artoolkitProfile.sourceWebcam(); // Is there good reason for having a function to set the sourceWebcam but not the displayWidth/Height etc?
-    
-    // add existing parameters, this is not well documented
-    let additionalParameters = {
-        // Device id of the camera to use (optional)
-        deviceId: null,
-        // resolution of at which we initialize in the source image
-        sourceWidth: 640,
-        sourceHeight: 480,
-        // resolution displayed for the source
-        displayWidth: 640,
-        displayHeight: 480,
-    }
-    
-    Object.assign(artoolkitProfile.sourceParameters, additionalParameters);
-    console.log(artoolkitProfile.sourceParameters); // now includes the additionalParameters
+		Object.assign(artoolkitProfile.sourceParameters, additionalParameters);
+		console.log(artoolkitProfile); // now includes the additionalParameters
 
-    const arToolkitSource = new ArToolkitSource(artoolkitProfile.sourceParameters);
+		const arToolkitSource = new ArToolkitSource(
+			artoolkitProfile.sourceParameters
+		);
 
-    arToolkitSource.init(function onReady(){
-      onResize();
-    })
+		arToolkitSource.init(function onReady() {
+			onResize();
+		});
 
-    // handle resize
-    window.addEventListener('resize', function(){
-      onResize(); 
-    })
+		// handle resize
+		window.addEventListener("resize", function () {
+			onResize();
+		});
 
-    // resize is not called for the canvas on init. The canvas with the cube seems to be resized correctly at start.
-    // Is that maybe a vue-specific problem?
-    function onResize(){
-      arToolkitSource.onResizeElement()
-      arToolkitSource.copyElementSizeTo(renderer.domElement)
-      if(arToolkitContext.arController !== null){
-        arToolkitSource.copyElementSizeTo(arToolkitContext.arController.canvas)
-      }
-    }
-      
+		// resize is not called for the canvas on init. The canvas with the cube seems to be resized correctly at start.
+		// Is that maybe a vue-specific problem?
+		function onResize() {
+			arToolkitSource.onResizeElement();
+			arToolkitSource.copyElementSizeTo(this.renderer.domElement);
+			if (arToolkitContext.arController !== null) {
+				arToolkitSource.copyElementSizeTo(
+					arToolkitContext.arController.canvas
+				);
+			}
+		}
 
-    ////////////////////////////////////////////////////////////////////////////////
-    //          initialize arToolkitContext
-    ////////////////////////////////////////////////////////////////////////////////
+		////////////////////////////////////////////////////////////////////////////////
+		//          initialize arToolkitContext
+		////////////////////////////////////////////////////////////////////////////////
 
-    // create atToolkitContext
-    var arToolkitContext = new ArToolkitContext({
-      debug: false,
-      cameraParametersUrl: ArToolkitContext.baseURL + 'data/camera_para.dat',
-      detectionMode: 'mono',
-      canvasWidth: 640,
-      canvasHeight: 490,
-      imageSmoothingEnabled : true, // There is still a warning about mozImageSmoothingEnabled when using Firefox
-    })
-    
-    // initialize it
-    arToolkitContext.init(function onCompleted(){
-      // copy projection matrix to camera
-      camera.projectionMatrix.copy( arToolkitContext.getProjectionMatrix() );
-    })
+		// create atToolkitContext
+		var arToolkitContext = new ArToolkitContext({
+			debug: false,
+			cameraParametersUrl:
+				ArToolkitContext.baseURL + "data/camera_para.dat",
+			detectionMode: "mono",
+			canvasWidth: 640,
+			canvasHeight: 490,
+			imageSmoothingEnabled: true, // There is still a warning about mozImageSmoothingEnabled when using Firefox
+		});
 
-    // update artoolkit on every frame
-    onRenderFcts.push(function(){
-      if( arToolkitSource.ready === false )	return
+		// initialize it
+		arToolkitContext.init(function onCompleted() {
+			// copy projection matrix to camera
+			camera.projectionMatrix.copy(
+				arToolkitContext.getProjectionMatrix()
+			);
+			console.log(arToolkitContext.getProjectionMatrix());
+		});
 
-      arToolkitContext.update( arToolkitSource.domElement )
-    })
+		// update artoolkit on every frame
+		onRenderFcts.push(function () {
+			if (arToolkitSource.ready === false) return;
 
-    ////////////////////////////////////////////////////////////////////////////////
-    //          Create a ArMarkerControls
-    ////////////////////////////////////////////////////////////////////////////////
+			arToolkitContext.update(arToolkitSource.domElement);
+		});
 
-    var markerGroup = new THREE.Group()
-    scene.add(markerGroup)
+		////////////////////////////////////////////////////////////////////////////////
+		//          Create a ArMarkerControls
+		////////////////////////////////////////////////////////////////////////////////
 
-    var markerControls = new ArMarkerControls(arToolkitContext, markerGroup, {
-      type: 'pattern',
-      patternUrl: ArToolkitContext.baseURL + 'data/patt.hiro',
-      smooth: true,
-      smoothCount: 5,
-      smoothTolerance: 0.01,
-      smoothThreshold: 2
-    })
+		var markerGroup = new THREE.Group();
+		this.scene.add(markerGroup);
 
-    //////////////////////////////////////////////////////////////////////////////////
-    //		add an object in the scene
-    //////////////////////////////////////////////////////////////////////////////////
+		var markerControls = new ArMarkerControls(
+			arToolkitContext,
+			markerGroup,
+			{
+				type: "pattern",
+				patternUrl: ArToolkitContext.baseURL + "data/patt.hiro",
+				smooth: true,
+				smoothCount: 5,
+				smoothTolerance: 0.01,
+				smoothThreshold: 2,
+			}
+		);
 
-    var markerScene = new THREE.Scene()
-    markerGroup.add(markerScene)
+		//////////////////////////////////////////////////////////////////////////////////
+		//		add an object in the scene
+		//////////////////////////////////////////////////////////////////////////////////
 
-    var mesh = new THREE.AxesHelper()
-    markerScene.add(mesh)
+		var markerScene = new THREE.Scene();
+		markerGroup.add(markerScene);
 
-    // add a torus knot
-    var geometry	= new THREE.BoxGeometry(1,1,1);
-    var material	= new THREE.MeshNormalMaterial({
-      transparent : true,
-      opacity: 0.5,
-      side: THREE.DoubleSide
-    });
-    var mesh	= new THREE.Mesh( geometry, material );
-    mesh.position.y	= geometry.parameters.height/2
-    markerScene.add(mesh)
+		var mesh = new THREE.AxesHelper();
+		markerScene.add(mesh);
 
-    var geometry	= new THREE.TorusKnotGeometry(0.3,0.1,64,16);
-    var material	= new THREE.MeshNormalMaterial();
-    var mesh	= new THREE.Mesh( geometry, material );
-    mesh.position.y	= 0.5
-    markerScene.add( mesh );
+		// add a torus knot
+		var geometry = new THREE.BoxGeometry(1, 1, 1);
+		var material = new THREE.MeshNormalMaterial({
+			transparent: true,
+			opacity: 0.5,
+			side: THREE.DoubleSide,
+		});
+		var mesh = new THREE.Mesh(geometry, material);
+		mesh.position.y = geometry.parameters.height / 2;
+		markerScene.add(mesh);
 
-    onRenderFcts.push(function(delta){
-      mesh.rotation.x += delta * Math.PI
-    })
+		var geometry = new THREE.TorusKnotGeometry(0.3, 0.1, 64, 16);
+		var material = new THREE.MeshNormalMaterial();
+		var mesh = new THREE.Mesh(geometry, material);
+		mesh.position.y = 0.5;
+		markerScene.add(mesh);
 
-    //////////////////////////////////////////////////////////////////////////////////
-    //		render the whole thing on the page
-    //////////////////////////////////////////////////////////////////////////////////
-    onRenderFcts.push(function(){
-      renderer.render(scene, camera);
-    })
+		onRenderFcts.push(function (delta) {
+			mesh.rotation.x += delta * Math.PI;
+		});
 
-      // run the rendering loop
-    var lastTimeMsec = null;
-    requestAnimationFrame(function animate(nowMsec){
-      // keep looping
-      requestAnimationFrame(animate);
-      // measure time
-      lastTimeMsec	= lastTimeMsec || nowMsec-1000/60
-      var deltaMsec	= Math.min(200, nowMsec - lastTimeMsec)
-      lastTimeMsec	= nowMsec
-      // call each update function
-      onRenderFcts.forEach(function(onRenderFct){
-        onRenderFct(deltaMsec/1000, nowMsec/1000)
-      })
-    })
-    }
-}
+		//////////////////////////////////////////////////////////////////////////////////
+		//		render the whole thing on the page
+		//////////////////////////////////////////////////////////////////////////////////
+		const renderer = this.renderer;
+		const scene = this.scene;
+		onRenderFcts.push(function () {
+			renderer.render(scene, camera);
+		});
+
+		// run the rendering loop
+		var lastTimeMsec = null;
+		requestAnimationFrame(function animate(nowMsec) {
+			// keep looping
+			requestAnimationFrame(animate);
+			// measure time
+			lastTimeMsec = lastTimeMsec || nowMsec - 1000 / 60;
+			var deltaMsec = Math.min(200, nowMsec - lastTimeMsec);
+			lastTimeMsec = nowMsec;
+			// call each update function
+			onRenderFcts.forEach(function (onRenderFct) {
+				onRenderFct(deltaMsec / 1000, nowMsec / 1000);
+			});
+		});
+		console.log("onRenderFcts");
+		console.log(onRenderFcts);
+	},
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
